75720092d1ff4daa86ecfef8953e5a610b1065c4
Daniil Zverev <zverev@in.tum.de>
2025-05-05 21:23:27 +0200

    Update local.sh for VGGSound processing: change dataset path, output CSV location, and pagination settings. Add new .gitkeep files for CSV directories.

--- local.sh
+++ local.sh
@@ -1,12 +1,13 @@
+mkdir -p /tmp/zverev/vggsound
 /usr/bin/squashfuse /dss/dssmcmlfs01/pn67gu/pn67gu-dss-0000/zverev/datasets/vggsound.squashfs /tmp/vggsound
 
 python process_vggsound.py \
     --tokenizer_path config/tokenizer.model \
-    --dataset_path /tmp/zverev/$SLURM_ARRAY_TASK_ID/vggsound \
+    --dataset_path /tmp/zverev/vggsound \
     --video_csv ../../data/train.csv \
-    --output_csv csv/$modality/predictions.csv \
-    --page $SLURM_ARRAY_TASK_ID \
-    --per_page 1000 \
-    --modality $modality \
+    --output_csv csv/av/predictions.csv \
+    --page 1 \
+    --per_page 10 \
+    --modality av \
     --prompt_mode multi \
     --prompt "Do you see {cl} class in this video? Answer only with yes or no."
