e267fd19d8d9cc9440e9bf0487bb0b785d653812
Daniil Zverev <zverev@in.tum.de>
2025-05-14 21:49:06 +0200

    Update SLURM script for VGGSound processing: increase node and task counts to 2, adjust GPU allocation to 2, reduce job time to 20 minutes, and unify modality prompt for video tasks. Modify task execution parameters for improved resource management.

--- process_vggsound_multiple_leo.sh
+++ process_vggsound_multiple_leo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name="jid:vla-fn48"
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1 
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=24
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --partition=boost_usr_prod
 #SBATCH --qos=boost_qos_dbg
 #SBATCH --mem=96GB
-#SBATCH --time=01:00:00
+#SBATCH --time=00:20:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=a-sophia.koepke@uni-tuebingen.de
 #SBATCH --output=./logs/slurm-%j.out
@@ -24,23 +24,25 @@ modality=$1
 echo "This is $modality, page 1"
 
 # Set the appropriate prompt based on the modality
-if [ "$modality" = "a" ]; then
-    PROMPT="Do you hear \"{cl}\" class in this audio? Answer only with yes or no."
-else
-    PROMPT="Do you see or hear \"{cl}\" class in this video? Answer only with yes or no."
-fi
+PROMPT="Do you see or hear \"{cl}\" class in this video? Answer only with yes or no."
 
-# Run the script on each node, assigning each task to a different GPU
-srun --exclusive --ntasks=1 python process_vggsound.py \
+SRUN_ARGS=" \
+    --wait=60 \
+    --kill-on-bad-exit=1 \
+    --jobid $SLURM_JOB_ID \
+    "
+
+srun $SRUN_ARGS bash -c " python process_vggsound.py \
     --tokenizer_path config/tokenizer.model \
     --dataset_path /leonardo_work/EUHPC_E03_068/akoepke/vs \
     --video_csv ../../data/test.csv \
     --output_csv csv/$modality/predictions.csv \
-    --page 1 \
-    --per_page 100 \
+    --page \$SLURM_PROCID \
+    --per_page 60 \
     --modality $modality \
     --prompt_mode multi \
-    --prompt "$PROMPT" \
+    --prompt \"$PROMPT\" \
     --batch_size 256 \
     --temperature 0.9 \
     --top_p 0.9
+    "
