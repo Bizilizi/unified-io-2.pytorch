c9ce88f11851281860d762a448ffd91c867e1543
Daniil Zverev <zverev@in.tum.de>
2025-05-14 21:46:02 +0200

    Update SLURM script for VGGSound processing: increase node count to 65, reduce cpus-per-task to 8, change qos to boost_qos_bprod, adjust memory allocation to 256G, and shorten job time to 20 minutes. Also, modify per_page parameter from 65 to 60 for optimized task distribution.

--- process_vggsound_multiple_leo_prod.sh
+++ process_vggsound_multiple_leo_prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name="jid:vla-fn48"
-#SBATCH --nodes=64
+#SBATCH --nodes=65
 #SBATCH --ntasks=260
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:4
 #SBATCH --partition=boost_usr_prod
-#SBATCH --qos=normal
-#SBATCH --mem=250G
-#SBATCH --time=8:00:00
+#SBATCH --qos=boost_qos_bprod
+#SBATCH --mem=256G
+#SBATCH --time=00:20:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=a-sophia.koepke@uni-tuebingen.de
 #SBATCH --output=./logs/slurm-%j.out
@@ -39,7 +39,7 @@ srun $SRUN_ARGS bash -c " python process_vggsound.py \
     --video_csv ../../data/test.csv \
     --output_csv csv/$modality/predictions.csv \
     --page \$SLURM_PROCID \
-    --per_page 65 \
+    --per_page 60 \
     --modality $modality \
     --prompt_mode multi \
     --prompt \"$PROMPT\" \
